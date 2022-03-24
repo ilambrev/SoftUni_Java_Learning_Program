3a57c49e15a00ffb6acb3fb57d1db6fdfad62480
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-03-24 09:57:05 +0200

    Add files via upload

---
+++ 01Programming_Basics_with_Java_2021_10_02/01First_Steps_In_Coding_Lab/P06ConcatenateData.java
@@ -0,0 +1,17 @@
+package firstSteps;
+
+import java.util.Scanner;
+
+public class P06ConcatenateData {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String town = scanner.nextLine();
+
+        System.out.println("You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + town + ".");
+    }
+}
