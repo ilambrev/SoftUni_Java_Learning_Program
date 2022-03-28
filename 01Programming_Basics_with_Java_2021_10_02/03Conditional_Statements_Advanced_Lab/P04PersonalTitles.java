3ad28a41aa776e77e694acb7796da075af6a46e0
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-03-28 08:07:36 +0300

    Add files via upload

---
+++ 01Programming_Basics_with_Java_2021_10_02/03Conditional_Statements_Advanced_Lab/P04PersonalTitles.java
@@ -0,0 +1,27 @@
+package conditionalStatementsAdvanced;
+
+import java.util.Scanner;
+
+public class P04PersonalTitles {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double age = Double.parseDouble(scanner.nextLine());
+        String sex = scanner.nextLine();
+
+        if (sex.equals("m")) {
+            if (age >= 16) {
+                System.out.println("Mr.");
+            } else if (age < 16) {
+                System.out.println("Master");
+            }
+        } else if (sex.equals("f")) {
+            if (age >= 16) {
+                System.out.println("Ms.");
+            } else if (age < 16) {
+                System.out.println("Miss");
+            }
+        }
+    }
+}
