34801a18523cd97ac6d42e6788b980b56c9e2cc1
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-03-30 08:34:36 +0300

    Add files via upload

---
+++ 01Programming_Basics_with_Java_2021_10_02/04For_Loop_Lab/P01NumbersFrom1To100.java
@@ -0,0 +1,10 @@
+package forLoop;
+
+public class P01NumbersFrom1To100 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
