0abb50c9901c6ee81c45d4bbd0f14d634d4a370e
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-03-31 10:07:47 +0300

    Add files via upload

---
+++ 01Programming_Basics_with_Java_2021_10_02/04For_Loop_Exercise/E01NumbersEndingIn7.java
@@ -0,0 +1,11 @@
+package e04ForLoop;
+
+public class E01NumbersEndingIn7 {
+    public static void main(String[] args) {
+
+
+        for (int i = 7; i <= 1000; i += 10) {
+            System.out.println(i);
+        }
+    }
+}
