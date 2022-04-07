bdb8f30e9c9529136cc8a142a49b34b2e6f9af01
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-04-07 13:08:18 +0300

    Add files via upload

---
+++ 02Programming_Fundamentals_with_Java_2022_01_10/01Basic_Syntax_Conditional_Statements_And_Loops_Lab/L06ForeignLanguages.java
@@ -0,0 +1,27 @@
+package L01BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class L06ForeignLanguages {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String country = scanner.nextLine();
+
+        switch (country) {
+            case "England":
+            case "USA":
+                System.out.println("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                System.out.println("Spanish");
+                break;
+            default:
+                System.out.println("unknown");
+                break;
+        }
+    }
+}
