b4fb43cddbea2a175dd66c65e83b9655c2479f00
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-04-08 11:07:06 +0300

    Add files via upload

---
+++ 02Programming_Fundamentals_with_Java_2022_01_10/01Basic_Syntax_Conditional_Statements_And_Loops_Lab/L08DivisibleBy3.java
@@ -0,0 +1,12 @@
+package L01BasicSyntaxConditionalStatementsAndLoops;
+
+public class L08DivisibleBy3 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
