8b711fdfbf8a3984d71764125549c6b0bcbe59bf
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-04-09 09:59:34 +0300

    Add files via upload

---
+++ 02Programming_Fundamentals_with_Java_2022_01_10/01Basic_Syntax_Conditional_Statements_And_Loops_Exercise/E01Ages.java
@@ -0,0 +1,24 @@
+package E01BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class E01Ages {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int age = scanner.nextInt();
+
+        if (age >= 0 && age <= 2) {
+            System.out.println("baby");
+        } else if (age <= 13) {
+            System.out.println("child");
+        } else if (age <= 19) {
+            System.out.println("teenager");
+        } else if (age <= 65) {
+            System.out.println("adult");
+        } else {
+            System.out.println("elder");
+        }
+    }
+}
