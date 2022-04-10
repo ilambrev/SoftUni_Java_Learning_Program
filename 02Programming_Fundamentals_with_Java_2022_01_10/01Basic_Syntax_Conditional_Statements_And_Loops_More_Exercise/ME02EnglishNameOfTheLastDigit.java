d6638dfc07cb73d16ca8dbcc46755239f9d8cfb3
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-04-10 10:04:25 +0300

    Add files via upload

---
+++ 02Programming_Fundamentals_with_Java_2022_01_10/01Basic_Syntax_Conditional_Statements_And_Loops_More_Exercise/ME02EnglishNameOfTheLastDigit.java
@@ -0,0 +1,50 @@
+package ME01BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class ME02EnglishNameOfTheLastDigit {
+    static void lastDigitName() {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        int lastDigit = num % 10;
+
+        switch (lastDigit) {
+            case 0:
+                System.out.println("zero");
+                break;
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("six");
+                break;
+            case 7:
+                System.out.println("seven");
+                break;
+            case 8:
+                System.out.println("eight");
+                break;
+            case 9:
+                System.out.println("nine");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        lastDigitName();
+    }
+}
