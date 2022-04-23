066ed61c05f326bd41f201484df4e1465f046273
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-04-23 14:52:28 +0300

    Add files via upload

---
+++ 02Programming_Fundamentals_with_Java_2022_01_10/06Objects_And_Classes_Lab/L04SongsV2.java
@@ -0,0 +1,78 @@
+package L06ObjectsAndClasses;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class L04SongsV2 {
+
+    public static class Song {
+        private String listType;
+        private String songName;
+        private String time;
+
+        public String getListType() {
+            return listType;
+        }
+
+        public void setListType(String listType) {
+            this.listType = listType;
+        }
+
+        public String getSongName() {
+            return songName;
+        }
+
+        public void setSongName(String songName) {
+            this.songName = songName;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfSongs = Integer.parseInt(scanner.nextLine());
+
+        List<Song> songsList = new ArrayList<>();
+
+        for (int i = 0; i < numberOfSongs; i++) {
+            String[] songString = scanner.nextLine().split("_");
+            String listType = songString[0];
+            String songName = songString[1];
+            String time = songString[2];
+
+            Song song = new Song();
+
+            song.setListType(listType);
+            song.setSongName(songName);
+            song.setTime(time);
+
+            songsList.add(song);
+        }
+
+        String songQuery = scanner.nextLine();
+
+        if (songQuery.equals("all")) {
+            for (Song song : songsList) {
+                System.out.println(song.getSongName());
+            }
+        }
+
+        else {
+            for (Song song : songsList) {
+                if (song.getListType().equals(songQuery)) {
+                    System.out.println(song.getSongName());
+                }
+            }
+        }
+    }
+}
