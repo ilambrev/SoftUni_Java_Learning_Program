02eb6149e4f42e94878374b1be060939ec1dfbd2
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-06-06 09:17:41 +0300

    Add files via upload

---
+++ 03Java_Advanced_2022_05_17/04Streams_Files_And_Directories_Exercises/E05LineNumbers.java
@@ -0,0 +1,31 @@
+package E04StreamsFilesAndDirectories;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class E05LineNumbers {
+    public static void main(String[] args) {
+
+        int counter = 1;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("src/EF04StreamsFilesAndDirectories/inputLineNumbers.txt"));
+             PrintWriter pw = new PrintWriter("src/EF04StreamsFilesAndDirectories/output.txt")) {
+
+            String line = br.readLine();
+
+            while (line != null) {
+                String newLineFormat = String.format("%d. %s", counter, line);
+                pw.println(newLineFormat);
+                counter++;
+                line = br.readLine();
+            }
+
+        } catch (
+                IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
