a43ba5f543def227e1480aed52da1671daddd891
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-06-11 12:05:35 +0300

    Add files via upload

---
+++ 03Java_Advanced_2022_05_17/06Defining_Classes_Exercises/02CompanyRoster/CompanyRoster/Employee.java
@@ -0,0 +1,60 @@
+package CompanyRoster;
+
+public class Employee {
+    private String name;
+    private double salary;
+    private String position;
+    private String department;
+    private String email;
+    private int age;
+
+
+    public Employee(String name, double salary, String position, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        this.email = "n/a";
+        this.age = -1;
+    }
+
+    public Employee(String name, double salary, String position, String department, String email) {
+        this(name, salary, position, department);
+        this.email = email;
+    }
+
+    public Employee(String name, double salary, String position, String department, int age) {
+        this(name, salary, position, department);
+        this.age = age;
+    }
+
+    public Employee(String name, double salary, String position, String department, String email, int age) {
+        this(name, salary, position, department, email);
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public String getPosition() {
+        return this.position;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+}
