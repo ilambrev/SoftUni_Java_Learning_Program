ba60efe663278a38c4899567757e999d377a1026
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-06-11 12:13:47 +0300

    Add files via upload

---
+++ 03Java_Advanced_2022_05_17/06Defining_Classes_Lab/L01CarInfo/CarInfo/Main.java
@@ -0,0 +1,28 @@
+package CarInfo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfCars = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= numberOfCars; i++) {
+            String[] carDetails = scanner.nextLine().split("\\s+");
+            String brand = carDetails[0];
+            String model = carDetails[1];
+            int horsePower = Integer.parseInt(carDetails[2]);
+
+            Car car = new Car();
+
+            car.setBrand(brand);
+            car.setModel(model);
+            car.setHorsePower(horsePower);
+
+            System.out.println(car.carInfo());
+        }
+
+    }
+}
