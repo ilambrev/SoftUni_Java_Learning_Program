e3009b7e6e96f892f478cf57fd4744c49fc7e8e8
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-06-25 07:54:26 +0300

    Add files via upload

---
+++ 03Java_Advanced_2022_05_17/10Exam_Preparation/06/EX03GroomingSalon/groomingSalon/GroomingSalon.java
@@ -0,0 +1,50 @@
+package groomingSalon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GroomingSalon {
+    private int capacity;
+    private List<Pet> data;
+
+    public GroomingSalon(int capacity) {
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public void add(Pet pet) {
+        if (this.data.size() < this.capacity) {
+            this.data.add(pet);
+        }
+    }
+
+    public boolean remove(String name) {
+        for (Pet pet : this.data) {
+            if (pet.getName().equals(name)) {
+                this.data.remove(pet);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Pet getPet(String name, String owner) {
+        for (Pet pet : this.data) {
+            if (pet.getName().equals(name) && pet.getOwner().equals(owner)) {
+                return pet;
+            }
+        }
+        return null;
+    }
+
+    public int getCount() {
+        return this.data.size();
+    }
+
+    public String getStatistics() {
+        return "The grooming salon has the following clients:" + System.lineSeparator() +
+                this.data.stream().map(pet -> pet.getName() + " " + pet.getOwner()).collect(Collectors.joining(System.lineSeparator()));
+    }
+
+}
---
+++ 03Java_Advanced_2022_05_17/10Exam_Preparation/06/EX03GroomingSalon/groomingSalon/Pet.java
@@ -0,0 +1,43 @@
+package groomingSalon;
+
+public class Pet {
+    private String name;
+    private int age;
+    private String owner;
+
+    public Pet(String name, int age, String owner) {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d - (%s)", this.name, this.age, this.owner);
+    }
+
+}
