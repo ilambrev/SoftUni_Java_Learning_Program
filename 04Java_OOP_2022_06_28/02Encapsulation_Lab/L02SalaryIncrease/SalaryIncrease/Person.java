52eab58719f12d312b417e05911ab0847717b2d9
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-07-03 10:25:59 +0300

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/02Encapsulation_Lab/L02SalaryIncrease/SalaryIncrease/Person.java
@@ -0,0 +1,52 @@
+package SalaryIncrease;
+
+import java.text.DecimalFormat;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double bonus) {
+        if (this.age < 30) {
+            this.setSalary(this.getSalary() + this.getSalary() * bonus / 200);
+        } else {
+            this.setSalary(this.getSalary() + this.getSalary() * bonus / 100);
+        }
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("0.0##");
+        return String.format("%s %s gets %s leva", this.getFirstName(), this.getLastName(), df.format(this.getSalary()));
+    }
+
+}
