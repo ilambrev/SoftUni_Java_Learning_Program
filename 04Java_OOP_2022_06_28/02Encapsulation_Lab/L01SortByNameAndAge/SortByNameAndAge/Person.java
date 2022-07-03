a26fe2cdc677f1bcc3da8ea383a63dd1def55e25
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-07-03 10:24:45 +0300

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/02Encapsulation_Lab/L01SortByNameAndAge/SortByNameAndAge/Person.java
@@ -0,0 +1,31 @@
+package SortByNameAndAge;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s is %d years old.", this.firstName, this.lastName, this.age);
+    }
+
+}
