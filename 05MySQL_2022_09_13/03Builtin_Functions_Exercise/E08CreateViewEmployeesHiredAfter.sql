a42398089eba9a2c30342960e872e7bb67b73f92
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-10-21 09:26:54 +0300

    Add files via upload

---
+++ 05MySQL_2022_09_13/03Builtin_Functions_Exercise/E08CreateViewEmployeesHiredAfter.sql
@@ -0,0 +1,8 @@
+USE `soft_uni`;
+
+CREATE VIEW `v_employees_hired_after_2000` AS
+SELECT `first_name`, `last_name`
+FROM `employees`
+WHERE YEAR(`hire_date`) > 2000;
+
+SELECT * FROM `v_employees_hired_after_2000`;
