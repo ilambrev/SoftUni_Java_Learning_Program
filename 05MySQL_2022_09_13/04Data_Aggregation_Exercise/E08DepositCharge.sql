07cc31710276bdd7f0e6ec3fde98e0ff7908cd42
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-10-23 08:13:00 +0300

    Add files via upload

---
+++ 05MySQL_2022_09_13/04Data_Aggregation_Exercise/E08DepositCharge.sql
@@ -0,0 +1,6 @@
+USE `gringotts`;
+
+SELECT `deposit_group`, `magic_wand_creator`, MIN(`deposit_charge`) AS `min_deposit_charge`
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`, `magic_wand_creator`
+ORDER BY `magic_wand_creator`, `deposit_group`;
