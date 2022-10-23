ffa9875940bfb6c83ccd94cd63ac508f2fdf602b
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-10-23 08:16:33 +0300

    Add files via upload

---
+++ 05MySQL_2022_09_13/04Data_Aggregation_Exercise/E14EmployeesMaximumSalaries.sql
@@ -0,0 +1,7 @@
+USE `soft_uni`;
+
+SELECT `department_id`, MAX(`salary`) AS `max_salary`
+FROM `employees`
+GROUP BY `department_id`
+HAVING `max_salary` NOT BETWEEN 30000 AND 70000
+ORDER BY `department_id`;
