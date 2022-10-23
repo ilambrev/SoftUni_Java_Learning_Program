39e349d895e7416b5c84188b48ddbacfff38bf73
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-10-23 08:12:25 +0300

    Add files via upload

---
+++ 05MySQL_2022_09_13/04Data_Aggregation_Exercise/E06DepositsSumForOllivanderFamily.sql
@@ -0,0 +1,7 @@
+USE `gringotts`;
+
+SELECT `deposit_group`, SUM(`deposit_amount`) AS `total_sum`
+FROM `wizzard_deposits`
+WHERE `magic_wand_creator` = "Ollivander family"
+GROUP BY `deposit_group`
+ORDER BY `deposit_group`;
