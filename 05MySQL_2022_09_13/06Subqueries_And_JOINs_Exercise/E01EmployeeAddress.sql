ec9157ce680e9aa2d64eccd810daae2b4d3206fd
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-10-27 10:03:48 +0300

    Add files via upload

---
+++ 05MySQL_2022_09_13/06Subqueries_And_JOINs_Exercise/E01EmployeeAddress.sql
@@ -0,0 +1,8 @@
+USE `soft_uni`;
+
+SELECT e.`employee_id`, e.`job_title`, e.`address_id`, a.`address_text`
+FROM `employees` AS e
+INNER JOIN `addresses` AS a
+ON e.`address_id` = a.`address_id`
+ORDER BY e.`address_id` ASC
+LIMIT 5;
