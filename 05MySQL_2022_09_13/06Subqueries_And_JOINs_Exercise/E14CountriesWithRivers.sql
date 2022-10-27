7c62ed0bbd20368228f0fe5cfb456971179589cd
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-10-27 10:09:31 +0300

    Add files via upload

---
+++ 05MySQL_2022_09_13/06Subqueries_And_JOINs_Exercise/E14CountriesWithRivers.sql
@@ -0,0 +1,11 @@
+USE `geography`;
+
+SELECT c.`country_name`, r.`river_name`
+FROM `countries` AS c
+LEFT JOIN `countries_rivers` AS cr
+ON c.`country_code` = cr.`country_code`
+LEFT JOIN `rivers` AS r
+ON r.`id` = cr.`river_id`
+WHERE c.`continent_code` = "AF"
+ORDER BY c.`country_name`
+LIMIT 5;
