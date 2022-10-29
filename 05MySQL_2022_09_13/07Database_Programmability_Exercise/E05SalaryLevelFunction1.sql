49122ad2741fa01ad235b8f6274ec243c19e7cdb
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-10-29 09:26:52 +0300

    Add files via upload

---
+++ 05MySQL_2022_09_13/07Database_Programmability_Exercise/E05SalaryLevelFunction1.sql
@@ -0,0 +1,17 @@
+USE `soft_uni`;
+
+DELIMITER $$
+
+CREATE FUNCTION ufn_get_salary_level1(employee_salary DECIMAL(19, 4))
+RETURNS VARCHAR(7)
+DETERMINISTIC
+BEGIN
+	DECLARE salary_level VARCHAR(7);
+	IF employee_salary < 30000 THEN SET salary_level := "Low";
+    ELSEIF employee_salary BETWEEN 30000 AND 50000 THEN SET salary_level := "Average";
+	ELSE SET salary_level := "High";
+    END IF;
+RETURN salary_level;
+END$$
+
+SELECT ufn_get_salary_level1(13500.00) AS 'salary_Level';
