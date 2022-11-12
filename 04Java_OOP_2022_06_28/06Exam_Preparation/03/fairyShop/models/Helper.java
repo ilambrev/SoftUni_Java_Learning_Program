a1983ed32340bf834cabab776e31995deea645eb
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-11-12 11:17:37 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/06Exam_Preparation/03/fairyShop/models/Helper.java
@@ -0,0 +1,19 @@
+package fairyShop.models;
+
+import java.util.Collection;
+
+public interface Helper {
+
+    void work();
+
+    void addInstrument(Instrument instrument);
+
+    boolean canWork();
+
+    String getName();
+
+    int getEnergy();
+
+    Collection<Instrument> getInstruments();
+
+}
