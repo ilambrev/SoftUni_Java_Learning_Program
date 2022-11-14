0241012ac6980432e6034fbc8cecadce073b3eb3
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-11-14 09:26:54 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/06Exam_Preparation/04/christmasRaces/repositories/CarRepository.java
@@ -0,0 +1,38 @@
+package christmasRaces.repositories;
+
+import christmasRaces.entities.cars.Car;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class CarRepository implements Repository<Car> {
+
+    private Collection<Car> cars;
+
+    public CarRepository() {
+        this.cars = new ArrayList<>();
+    }
+
+
+    @Override
+    public Car getByName(String name) {
+        return this.cars.stream().filter(c -> c.getModel().equals(name)).findFirst().orElse(null);
+    }
+
+    @Override
+    public Collection<Car> getAll() {
+        return Collections.unmodifiableCollection(this.cars);
+    }
+
+    @Override
+    public void add(Car model) {
+        this.cars.add(model);
+    }
+
+    @Override
+    public boolean remove(Car model) {
+        return this.cars.remove(model);
+    }
+
+}
