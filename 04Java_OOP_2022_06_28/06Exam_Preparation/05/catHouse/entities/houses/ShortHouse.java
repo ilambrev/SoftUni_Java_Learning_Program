14574842a161fdbbe45cc1390fcdd0ab45888e85
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-11-16 08:57:04 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/06Exam_Preparation/05/catHouse/entities/houses/ShortHouse.java
@@ -0,0 +1,11 @@
+package catHouse.entities.houses;
+
+public class ShortHouse extends BaseHouse {
+
+    private static final int CAPACITY = 15;
+
+    public ShortHouse(String name) {
+        super(name, CAPACITY);
+    }
+
+}
