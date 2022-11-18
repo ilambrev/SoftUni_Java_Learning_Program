aa39a9232a153501796439be3305bb8fbd44c9aa
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-11-18 08:47:35 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/06Exam_Preparation/06/glacialExpedition/models/explorers/AnimalExplorer.java
@@ -0,0 +1,11 @@
+package glacialExpedition.models.explorers;
+
+public class AnimalExplorer extends BaseExplorer {
+
+    private static final double ENERGY = 100.0;
+
+    public AnimalExplorer(String name) {
+        super(name, ENERGY);
+    }
+
+}
