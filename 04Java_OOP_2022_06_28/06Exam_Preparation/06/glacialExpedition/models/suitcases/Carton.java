aca6ea2f465ce5d8f92b9735dfb450ada5c0440c
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-11-18 08:50:53 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/06Exam_Preparation/06/glacialExpedition/models/suitcases/Carton.java
@@ -0,0 +1,19 @@
+package glacialExpedition.models.suitcases;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Carton implements Suitcase {
+
+    private Collection<String> exhibits;
+
+    public Carton() {
+        this.exhibits = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<String> getExhibits() {
+        return this.exhibits;
+    }
+
+}
