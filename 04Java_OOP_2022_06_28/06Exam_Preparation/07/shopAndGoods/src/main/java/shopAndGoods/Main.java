8513909c04c5d9bf0ed0519694686217bcc66aaa
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-11-21 08:49:26 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/06Exam_Preparation/07/shopAndGoods/src/main/java/shopAndGoods/Main.java
@@ -0,0 +1,9 @@
+package shopAndGoods;
+
+import javax.naming.OperationNotSupportedException;
+
+public class Main {
+    public static void main(String[] args) throws OperationNotSupportedException {
+
+    }
+}
