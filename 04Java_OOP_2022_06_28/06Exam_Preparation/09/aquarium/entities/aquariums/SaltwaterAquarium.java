14a38b6497167c6cdaf530ca005192014e1d6f99
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-11-24 15:47:24 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/06Exam_Preparation/09/aquarium/entities/aquariums/SaltwaterAquarium.java
@@ -0,0 +1,11 @@
+package aquarium.entities.aquariums;
+
+public class SaltwaterAquarium extends BaseAquarium {
+
+    private static final int CAPACITY = 25;
+
+    public SaltwaterAquarium(String name) {
+        super(name, CAPACITY);
+    }
+
+}
