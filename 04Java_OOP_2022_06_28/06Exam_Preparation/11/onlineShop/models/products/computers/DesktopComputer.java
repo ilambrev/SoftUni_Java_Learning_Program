c0504df05af3b2378058f096d84dffdd1a99ab81
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-12-07 22:22:48 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/06Exam_Preparation/11/onlineShop/models/products/computers/DesktopComputer.java
@@ -0,0 +1,11 @@
+package onlineShop.models.products.computers;
+
+public class DesktopComputer extends BaseComputer {
+
+    private static final double OVERALL_PERFORMANCE = 15.00;
+
+    public DesktopComputer(int id, String manufacturer, String model, double price) {
+        super(id, manufacturer, model, price, OVERALL_PERFORMANCE);
+    }
+
+}
---
+++ 04Java_OOP_2022_06_28/06Exam_Preparation/11/onlineShop/models/products/computers/Laptop.java
@@ -0,0 +1,11 @@
+package onlineShop.models.products.computers;
+
+public class Laptop extends BaseComputer {
+
+    private static final double OVERALL_PERFORMANCE = 10.00;
+
+    public Laptop(int id, String manufacturer, String model, double price) {
+        super(id, manufacturer, model, price, OVERALL_PERFORMANCE);
+    }
+
+}
