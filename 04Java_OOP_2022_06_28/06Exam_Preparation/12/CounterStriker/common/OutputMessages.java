83b01e3bfa700f2edb19c94152dc28a2cbd7825e
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2022-12-11 11:05:33 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/06Exam_Preparation/12/CounterStriker/common/OutputMessages.java
@@ -0,0 +1,11 @@
+package CounterStriker.common;
+
+public class OutputMessages {
+    public static final String SUCCESSFULLY_ADDED_PLAYER = "Successfully added player %s.";
+
+    public static final String SUCCESSFULLY_ADDED_GUN = "Successfully added gun %s.";
+
+    public static final String TERRORIST_WINS = "Terrorist wins!";
+
+    public static final String COUNTER_TERRORIST_WINS = "Counter Terrorist wins!";
+}
