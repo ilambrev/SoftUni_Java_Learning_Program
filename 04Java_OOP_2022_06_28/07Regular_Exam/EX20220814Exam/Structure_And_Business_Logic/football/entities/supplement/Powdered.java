73b5ed4d99bc76ec33a063fa4a178944ae2004a3
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-03-05 12:28:08 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/07Regular_Exam/EX20220814Exam/Structure_And_Business_Logic/football/entities/supplement/Powdered.java
@@ -0,0 +1,13 @@
+package football.entities.supplement;
+
+public class Powdered extends BaseSupplement {
+
+    private static final int ENERGY = 120;
+    private static final double PRICE = 15.00;
+
+
+    public Powdered() {
+        super(ENERGY, PRICE);
+    }
+
+}
