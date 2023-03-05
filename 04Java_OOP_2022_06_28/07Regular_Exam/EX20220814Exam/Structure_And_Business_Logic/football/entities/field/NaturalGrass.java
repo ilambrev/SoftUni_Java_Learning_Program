143254b858857355d3e455463e2bfae5aa9c314a
Ivan Lambrev <102065141+ilambrev@users.noreply.github.com>
2023-03-05 12:24:11 +0200

    Add files via upload

---
+++ 04Java_OOP_2022_06_28/07Regular_Exam/EX20220814Exam/Structure_And_Business_Logic/football/entities/field/NaturalGrass.java
@@ -0,0 +1,11 @@
+package football.entities.field;
+
+public class NaturalGrass extends BaseField {
+
+    private static final int CAPACITY = 150;
+
+    public NaturalGrass(String name) {
+        super(name, CAPACITY);
+    }
+
+}
