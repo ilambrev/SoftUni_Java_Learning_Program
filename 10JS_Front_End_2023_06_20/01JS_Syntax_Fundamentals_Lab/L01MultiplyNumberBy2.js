d64f629988c3e8168e18b61b4978de854f0ca834
ilambrev <ilambrev@abv.bg>
2023-06-20 21:36:42 +0300

    Multiply The Number By 2

---
+++ 10JS_Front_End_2023_06_20/01JS_Syntax_Fundamentals_Lab/L01MultiplyNumberBy2.js
@@ -0,0 +1,11 @@
+function multiplyNumberBy2(number) {
+
+    let result = number * 2;
+
+    console.log(result);
+
+}
+
+// multiplyNumberBy2(2);
+// multiplyNumberBy2(5);
+// multiplyNumberBy2(20);
