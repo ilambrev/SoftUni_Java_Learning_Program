b0b5fecd6859df19125c031df25341dddbd21fc3
ilambrev <ilambrev@abv.bg>
2023-06-22 08:27:59 +0300

    Sum First And Last Array Elements

---
+++ 10JS_Front_End_2023_06_20/01JS_Syntax_Fundamentals_Lab/L11SumFirstAndLastArrayElements.js
@@ -0,0 +1,11 @@
+function sumFirstAndLastArrayElements(numbers) {
+
+    let sum = numbers[0] + numbers[numbers.length - 1];
+
+    console.log(sum);
+
+}
+
+// sumFirstAndLastArrayElements([20, 30, 40]);
+// sumFirstAndLastArrayElements([10, 17, 22, 33]);
+// sumFirstAndLastArrayElements([11, 58, 69]);
