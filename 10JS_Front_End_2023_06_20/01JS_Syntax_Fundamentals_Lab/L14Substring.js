e2f98517545cb0134260fb50bd253b11428587a1
ilambrev <ilambrev@abv.bg>
2023-06-22 08:32:33 +0300

    Substring

---
+++ 10JS_Front_End_2023_06_20/01JS_Syntax_Fundamentals_Lab/L14Substring.js
@@ -0,0 +1,8 @@
+function substring(string, startingIndex, numberOfElements) {
+
+    console.log(string.substring(startingIndex, startingIndex + numberOfElements));
+
+}
+
+// substring('ASentence', 1, 8);
+// substring('SkipWord', 4, 7);
