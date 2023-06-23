31bf7377c0e996be4b1feeb1e27f94bfae556abc
ilambrev <ilambrev@abv.bg>
2023-06-23 10:20:10 +0300

    Ages

---
+++ 10JS_Front_End_2023_06_20/01JS_Syntax_Fundamentals_Exercises/E01Ages.js
@@ -0,0 +1,26 @@
+function ages(age) {
+
+    let output = '';
+
+    if (age >= 0 && age <= 2) {
+        output = 'baby';
+    } else if (age >= 3 && age <= 13) {
+        output = 'child';
+    } else if (age >= 14 && age <= 19) {
+        output = 'teenager';
+    } else if (age >= 20 && age <= 65) {
+        output = 'adult';
+    } else if (age >= 66) {
+        output = 'elder';
+    } else {
+        output = 'out of bounds';
+    }
+
+    console.log(output);
+
+}
+
+// ages(20);
+// ages(1);
+// ages(100);
+// ages(-1);
