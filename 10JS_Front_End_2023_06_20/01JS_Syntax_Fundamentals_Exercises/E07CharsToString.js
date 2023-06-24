8391947a83b2612ce86b4f946c0fb02a74f663c8
ilambrev <ilambrev@abv.bg>
2023-06-24 10:29:51 +0300

    Chars To String

---
+++ 10JS_Front_End_2023_06_20/01JS_Syntax_Fundamentals_Exercises/E07CharsToString.js
@@ -0,0 +1,10 @@
+function charsToString(firstChar, secondChar, thirdChar) {
+
+    let resultString = firstChar + secondChar + thirdChar;
+    console.log(resultString);
+
+}
+
+// charsToString('a', 'b', 'c');
+// charsToString('%', '2', 'o');
+// charsToString('1', '5', 'p');
