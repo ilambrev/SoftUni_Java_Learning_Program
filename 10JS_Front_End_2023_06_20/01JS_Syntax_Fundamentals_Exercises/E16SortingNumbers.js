8cb387af1cb4ad22d74aea3e26563e95d276895c
ilambrev <ilambrev@abv.bg>
2023-06-25 10:32:21 +0300

    Sorting Numbers

---
+++ 10JS_Front_End_2023_06_20/01JS_Syntax_Fundamentals_Exercises/E16SortingNumbers.js
@@ -0,0 +1,24 @@
+function sortingNumbers(numbers) {
+
+    let sortedNumbers = numbers.sort((n1, n2) => n1 - n2);
+    let resultArr = [];
+
+    for (let i = 0; i < sortedNumbers.length; i++) {
+
+        if (i % 2 === 0) {
+
+            resultArr.push(sortedNumbers[i / 2]);
+
+        } else {
+
+            resultArr.push(sortedNumbers[sortedNumbers.length - Math.ceil(i / 2)]);
+
+        }
+
+    }
+
+    return resultArr;
+
+}
+
+// console.log(sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
