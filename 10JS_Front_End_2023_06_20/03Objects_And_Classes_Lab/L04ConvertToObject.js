ab085f3b6754dadd5e2a089ce713311108824859
ilambrev <ilambrev@abv.bg>
2023-07-02 11:26:05 +0300

    Convert To Object

---
+++ 10JS_Front_End_2023_06_20/03Objects_And_Classes_Lab/L04ConvertToObject.js
@@ -0,0 +1,12 @@
+function convertToObject(jsonString) {
+
+    let person = JSON.parse(jsonString);
+
+    for (let [key, value] of Object.entries(person)) {
+        console.log(`${key}: ${value}`);
+    }
+
+}
+
+// convertToObject('{"name": "George", "age": 40, "town": "Sofia"}');
+// convertToObject('{"name": "Peter", "age": 35, "town": "Plovdiv"}');
