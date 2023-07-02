98611445626c8920604178a870c7cfbb30378de3
ilambrev <ilambrev@abv.bg>
2023-07-02 11:14:44 +0300

    City Taxes

---
+++ 10JS_Front_End_2023_06_20/03Objects_And_Classes_Lab/L03CityTaxes.js
@@ -0,0 +1,35 @@
+function cityTaxes(name, population, treasury) {
+
+    let city = {
+        name: name,
+        population: population,
+        treasury: treasury,
+        taxRate: 10,
+
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate;
+        },
+
+        applyGrowth(percentage) {
+            this.population += this.population * (percentage / 100);
+        },
+
+        applyRecession(percentage) {
+            this.treasury -= this.treasury * (percentage / 100);
+        }
+    }
+
+    return city;
+}
+
+// const city = cityTaxes('Tortuga', 7000, 15000);
+
+// city.collectTaxes();
+
+// console.log(city.treasury);
+
+// city.applyGrowth(5);
+
+// console.log(city.population);
+
+// console.log(city);
