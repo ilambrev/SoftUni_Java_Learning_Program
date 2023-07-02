cbec030fc00f59507aa6655232a8cc60b9a70400
ilambrev <ilambrev@abv.bg>
2023-07-02 11:11:29 +0300

    Person Info

---
+++ 10JS_Front_End_2023_06_20/03Objects_And_Classes_Lab/L01PersonInfo.js
@@ -0,0 +1,14 @@
+function personInfo(firstName, lastName, age) {
+
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    }
+
+    return person;
+
+}
+
+// personInfo("Peter", "Pan", "20");
+// personInfo("George", "Smith", "18");
